b5c1b50dd82c9f7b2e4baf582f6240de98e1351f
Yuki-Kanamori <y.kanamory@gmail.com>
2025-03-26 08:16:36 +0900

    intial commit

---
+++ detect.R
@@ -0,0 +1,44 @@
+require(tidyverse)
+require(heatwaveR)
+
+dir = "/Users/Yuki/Dropbox/SST/all/SSTまとめ"
+setwd(dir)
+sst = read_csv("sst_1982-2023.csv")
+
+head(sst)
+lonlat = data_frame(lon = sst$lon, lat = sst$lat) %>% mutate(tag = paste(lon, lat, sep = "_")) %>% distinct(tag, .keep_all = TRUE)
+
+mhws = NULL
+ for(i in 1:nrow(lonlat)){
+   tag = lonlat[i, ]
+   tag_lon = tag$lon
+   tag_lat = tag$lat
+   df = sst %>% filter(lon == tag_lon, lat == tag_lat)
+   df = df %>% mutate(temp = paste(year, month, day, sep = "-")) %>% mutate(date = as.Date(temp)) %>% select(-temp)
+   res = detect_event(ts2clm(data = df,
+                             y = SST,
+                             x = date,
+                             climatologyPeriod = c("1982-01-01", "2012-12-31")))
+   cate = category(res, S = FALSE, name = "doto") %>% select(event_no, category, season)
+   df_event = left_join(res_doto[["event"]], cate, by = "event_no") %>% mutate(lon = tag$lon, lat = tag$lat, tag = tag$tag)
+   
+   mhws = rbind(mhws, df_event)
+ }
+
+mcss = NULL
+for(i in 1:nrow(lonlat)){
+  tag = lonlat[i, ]
+  tag_lon = tag$lon
+  tag_lat = tag$lat
+  df = sst %>% filter(lon == tag_lon, lat == tag_lat)
+  df = df %>% mutate(temp = paste(year, month, day, sep = "-")) %>% mutate(date = as.Date(temp)) %>% select(-temp)
+  res = detect_event(ts2clm(data = df,
+                            y = SST,
+                            x = date,
+                            pctile = 10,
+                            climatologyPeriod = c("1982-01-01", "2012-12-31")))
+  cate = category(res, S = FALSE, name = "doto") %>% select(event_no, category, season)
+  df_event = left_join(res_doto[["event"]], cate, by = "event_no") %>% mutate(lon = tag$lon, lat = tag$lat, tag = tag$tag)
+  
+  mcss = rbind(mcss, df_event)
+}
