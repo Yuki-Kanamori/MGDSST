1cd28f09051a84566def4a38b94722c77d162af8
Yuki-Kanamori <y.kanamory@gmail.com>
2025-03-26 10:24:09 +0900

    revised

--- detect.R
+++ detect.R
@@ -1,5 +1,8 @@
 require(tidyverse)
 require(heatwaveR)
+require(maps)
+require(mapdata)
+require(mapproj)
 
 
 # directories -------------------------------------------------------------
@@ -10,6 +13,7 @@ dir_output = "/Users/Yuki/Dropbox/SST/MHWs"
 # sst data ----------------------------------------------------------------
 setwd(dir)
 sst = read_csv("sst_1982-2023.csv")
+summary(sst)
 
 
 # 異常値の検出 ------------------------------------------------------------------
@@ -62,8 +66,32 @@ for(i in 1:nrow(lonlat)){
 
 
 # 発生率と強度の算出 ---------------------------------------------------------------
-sum = mhws %>% mutate(year = str_sub(date_start, 1, 4)) 
-sum_h = sum %>% group_by(year, tag_no) %>% summarize(occu_rate = sum(duration), intensity = sum(intensity_cumulative))
+sum_h = mhws %>% mutate(year = str_sub(date_start, 1, 4)) 
+sum_h = sum_h %>% group_by(year, tag_no) %>% summarize(occu_rate = sum(duration), intensity = sum(intensity_cumulative))
+sum_h = left_join(sum_h, lonlat %>% mutate(tag_no = as.character(tag_no)), by = "tag_no")
 
-sum = mhws %>% mutate(year = str_sub(date_start, 1, 4)) 
-sum_h = sum %>% group_by(year, tag_no) %>% summarize(occu_rate = sum(duration), intensity = sum(intensity_cumulative))
+sum_c = mcss %>% mutate(year = str_sub(date_start, 1, 4)) 
+sum_c = sum_c %>% group_by(year, tag_no) %>% summarize(occu_rate = sum(duration), intensity = sum(intensity_cumulative))
+sum_c = left_join(sum_c, lonlat %>% mutate(tag_no = as.character(tag_no)), by = "tag_no")
+
+
+# 地図 ----------------------------------------------------------------------
+# 日本地図の設定（変更しない）
+japan = map_data("japan") %>% mutate(long = long - 0.01, lat = lat - 0.01)
+
+g = ggplot() + 
+  geom_polygon(data = japan, aes(x = long, y = lat, group = group), colour = "gray 50", fill = "gray 50")
+
+# sstデータの地点
+p = geom_point(data = lonlat, aes(x = lon, y = lat), shape = 15, size = 1)
+labs = labs(title = "", x = "", y = "")
+fig = g+p+labs+theme_bw()
+
+
+p = geom_point(data = sum_h, aes(x = lon, y = lat, color = occu_rate), shape = 15, size = 1)
+c = scale_color_gradientn(colours = c("blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
+f = facet_wrap(~ year, ncol = 6)
+labs = labs(title = "", x = "", y = "", fill = "days")
+
+fig = g+p+c+f+labs+theme_bw()
+ggsave(filename = "mhws_days.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
---
+++ getJapan.R
@@ -0,0 +1,68 @@
+require(tidyverse)
+require(stringr)
+
+# make dataframe ----------------------------------------------------------
+setwd("/Users/Yuki/Dropbox/SST/all")
+dir = "/Users/Yuki/Dropbox/SST/all"
+widths = rep(3, each = 1440)
+
+
+fl = list.files(path = dir, full.names = T, pattern = "txt") #読み込むファイル名のリスト化
+length(fl)
+
+
+length(fl)
+fl[1]
+fl[length(fl)]
+
+#####データの緯度経度を指定する#####
+#MGDSSTのlat
+lat = data.frame(seq(89.875, -89.875, -0.25))
+colnames(lat) = "lat"
+lat$tag = rep(1:720)
+#欲しい緯度範囲
+lat2 = lat[lat$lat >= 24.75 & lat$lat <= 48.75, ]
+
+
+#MGDSSTのlon
+lon = data.frame(seq(359.875, 0, -0.25))
+colnames(lon) = "lon"
+lon$tag = rep(1:1440)
+#欲しい経度範囲
+lon2 = lon[lon$lon >= 122.25 & lon$lon <= 150.25, ] # 小さくした
+
+
+# ディレクトリの作成 ---------------------------------------------------------------
+for(i in 1982:2024){
+  setwd(dir = "/Users/Yuki/Dropbox/SST/all/japan/")
+  dir.create(paste(i))
+}
+
+
+
+# データフレームの作成 --------------------------------------------------------------
+setwd("/Users/Yuki/Dropbox/SST/all")
+for(i in 1:length(fl)){
+  df = read_fwf(file = fl[i], fwf_widths(col_names = T, widths = widths), skip = 1)
+  df2 = df[min(lat2$tag):max(lat2$tag), min(lon2$tag):max(lon2$tag)]
+  colnames(df2) = lon2$lon
+  df2$lat = rep(lat2$lat)
+  df2$ymd = rep(str_sub(fl[i], (nchar(fl[i])-11), (nchar(fl[i])-4)), each = nrow(df2))
+  
+  year = str_sub(fl[i], -12, -9)
+  dir_write = paste("/Users/Yuki/Dropbox/SST/all/japan/", year, "/", sep = "", collapse ="")
+  setwd(dir_write)
+  getwd()
+  write.csv(df2, paste0(str_sub(fl[i], -25, -5), "_japan.csv"))
+}
+
+
+
+
+
+
+
+
+
+
+
